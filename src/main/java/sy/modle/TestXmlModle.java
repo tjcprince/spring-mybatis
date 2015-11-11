25f1b7c48ffc835770dc9e73783b75cfb04bf669
tjcprince <tan_jianchao@163.com>
2015-11-11 17:41:26 +0800

    初始提交

---
+++ src/main/java/sy/modle/TestXmlModle.java
@@ -0,0 +1,27 @@
+package sy.modle;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class TestXmlModle {
+	
+	private String id;
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	private String name;
+}
